8eb71ff62ba910fc2f5a6d8b18acc6334949b385
JuliaNorki <109559162+JuliaNorki@users.noreply.github.com>
2022-11-27 14:45:21 -0800

    1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,20 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog") // связываю два бина
+
     private Animal animal;
+    @Autowired
+    private Timer timer;
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +22,11 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+    public Timer getTimer() {
+        return timer;
+    }
+    public void setTimer(Timer timer) {
+        this.timer = timer;
     }
 }
